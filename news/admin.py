050d3af90594a4279662a7a0adb280f09d184bfe
MonkeyBusiness <osenchyn@gmail.com>
2022-05-29 08:28:15 +0300

    29.05.2022-08:28

--- news/admin.py
+++ news/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Donation, News
+from .models import Donation, News, Projects
 
 
 admin.site.register(News)
 admin.site.register(Donation)
+admin.site.register(Projects)
 
 
 
